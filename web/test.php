7da576a5d8216a0741907b2101753b7e9bb43528
shafiiiqahz <20037987@myrp.edu.sg>
2022-07-04 12:55:02 +0800

    commit 4

--- web/test.php
+++ web/test.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+<meta name="description" content="test page google lighthouse">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Test Page</title>
 </head>
